c639c54b171e5082b2f558ec18412d60b2ec7caa
xshapira <max@winoutt.com>
2024-01-04 15:52:20 +0200

    add ReservationDetailView

--- reservations/views.py
+++ reservations/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.views.generic import DetailView
 
-# Create your views here.
+from reservations.models import Reservation
+
+
+class ReservationDetailView(DetailView):
+    model = Reservation
+    template_name = "templates/index.html"
+
+    def get_context_data(self, **kwargs):
+        """
+        Returns the context data for a view.
+        """
+        return super().get_context_data(**kwargs)
