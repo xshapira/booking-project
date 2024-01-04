72677fc89b993a25f58d12d634b5cfcd43d79796
xshapira <max@winoutt.com>
2024-01-04 15:37:54 +0200

    register Person and Reservation models

--- reservations/admin.py
+++ reservations/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from reservations.models import Reservation
+
+
+@admin.register(Reservation)
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+
+
+@admin.register(Reservation)
+class ReservationAdmin(admin.ModelAdmin):
+    list_display = (
+        "person",
+        "date",
+        "room",
+    )
